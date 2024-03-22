1702cc5e1c4c9b64fcb2c48d2316cdf89cd8c4f0
Jack Fox <0xdeadbeef1@gmail.com>
2024-03-21 18:16:34 -0700

    Allow for force updating states

--- lib/Internal.lua
+++ lib/Internal.lua
@@ -138,8 +138,8 @@ return function(Iris: Types.Iris): Types.Internal
         
         Allows the caller to assign the state object a new value, and returns the new value.
     ]=]
-    function StateClass:set<T>(newValue: T): T
-        if newValue == self.value then
+    function StateClass:set<T>(newValue: T, force: boolean?): T
+        if newValue == self.value and force ~= true then
             -- no need to update on no change.
             return self.value
         end
