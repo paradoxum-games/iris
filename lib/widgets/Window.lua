8cdfb0bd95a26d9370b41bd45a40716c36cc8297
Jack Fox <0xdeadbeef1@gmail.com>
2024-03-21 17:44:33 -0700

    Fix inaccurate window frame size

--- lib/widgets/Window.lua
+++ lib/widgets/Window.lua
@@ -396,6 +396,8 @@ return function(Iris: Types.Internal, widgets: Types.WidgetUtility)
             end
             Window.Name = "Iris_Window"
 
+            local TitleButtonSize: number = Iris._config.TextSize + ((Iris._config.FramePadding.Y - 1) * 2)
+
             local WindowButton: TextButton = Instance.new("TextButton")
             WindowButton.Name = "WindowButton"
             WindowButton.Size = UDim2.fromOffset(0, 0)
@@ -449,7 +451,7 @@ return function(Iris: Types.Internal, widgets: Types.WidgetUtility)
             local ChildContainer: ScrollingFrame = Instance.new("ScrollingFrame")
             ChildContainer.Name = "ChildContainer"
             ChildContainer.AutomaticSize = Enum.AutomaticSize.None
-            ChildContainer.Size = UDim2.fromScale(1, 1)
+            ChildContainer.Size = UDim2.new(1, 0, 1, -TitleButtonSize)
             ChildContainer.Position = UDim2.fromOffset(0, 0)
             ChildContainer.BackgroundColor3 = Iris._config.WindowBgColor
             ChildContainer.BackgroundTransparency = Iris._config.WindowBgTransparency
@@ -522,7 +524,6 @@ return function(Iris: Types.Internal, widgets: Types.WidgetUtility)
                 end
             end)
 
-            local TitleButtonSize: number = Iris._config.TextSize + ((Iris._config.FramePadding.Y - 1) * 2)
 
             local CollapseButton: TextButton = Instance.new("TextButton")
             CollapseButton.Name = "CollapseButton"
